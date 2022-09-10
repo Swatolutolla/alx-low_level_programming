d39532a89f10c782e1eb0b56bdc586fb90a5f131
Tola Chris Olarinre <olutolachris@gmail.com>
2022-09-09 17:54:07 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a = 'a';
+		do {
+			putchar(a);
+			a++;
+		} while (a < 123);
+	return (0);
+}
