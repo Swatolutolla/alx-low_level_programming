fea136be7059f1da80fe6af7a6886c3e1974b9e4
Tola Chris Olarinre <olutolachris@gmail.com>
2022-09-09 19:32:39 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 0;
+
+	do {
+		printf("%d", a);
+			a++;
+	} while (a < 10);
+	putchar('\n');
+	return (0);
+}
