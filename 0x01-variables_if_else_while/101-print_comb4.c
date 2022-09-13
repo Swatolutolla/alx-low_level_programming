703c7bfa1377ecd17083fe813fc9ac3d9fcf6d59
Tola Chris Olarinre <olutolachris@gmail.com>
2022-09-12 22:05:47 -0700

    comb

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = '0';
+	int j = '0';
+	int k = '0';
+
+	for ( ; i <= 55; i++)
+	{
+		for (j = i + 1; j <= 56; j++)
+		{
+			for (2 + i; k <= 57; k++)
+			{
+
+			if (j == i || j == k || i == k)
+			{
+				k = k + 1;
+			}
+			putchar(i);
+			putchar(j);
+			putchar(k);
+			if (i <= 54)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
