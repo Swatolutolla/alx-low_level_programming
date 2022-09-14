65d359433c6b2b4dd2fd2298c755043f42fb5c52
Tola Chris Olarinre <olutolachris@gmail.com>
2022-09-14 03:53:44 -0700

    alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - print all english alphabets 10 times
+*
+* Return: Always Success
+*/
+void print_alphabet_x10(void)
+{
+	for (int a = 0; a < 10; a++)
+	{
+		print_alphabet();
+	}
+	_putchar('\n');
+}
+
