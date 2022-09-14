33bca8cfb653f2368dd9c98c4950cf8e68349b71
Tola Chris Olarinre <olutolachris@gmail.com>
2022-09-14 16:27:11 -0700

    lastdigit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
 * print_last_digit - print the last digit of a number
 *
 *@j: is the number whose last digit will be printed
-* Return: Always 0 Success
+* Return: Always
 */
 int print_last_digit(int j)
 {
