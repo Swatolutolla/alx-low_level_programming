82c7edd7bc74f70a3990bdc38354a72da288ce1b
Tola Chris Olarinre <olutolachris@gmail.com>
2022-09-15 15:13:06 -0700

    isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,17 +3,16 @@
 #include <stdio.h>
 
 int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jact_bauer(void);
-void times_table(void);
-int add(int, int);
-void prirt_to_98(int n);
-void print_times_table(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_traingle(int size);
+void print_number(int n);
 
 #endif 
