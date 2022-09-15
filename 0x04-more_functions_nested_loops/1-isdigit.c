5fb5be901538422b2deb4d4d8b262b4aab4848d6
Tola Chris Olarinre <olutolachris@gmail.com>
2022-09-15 15:17:52 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _isupper - checks if a charactewr is upper or lower case
+*
+*@c: is the character to be checked
+* Return: Always Success
+*/
+int _isupper(int c)
+{
+		if (c > 47 && c < 58)
+		{
+			c = 1;
+		}
+		else
+		{
+			c = 0;
+		}
+		return (c);
+}
