d91fc19c4bb30b5c3ae8dfc047c8e6e86d9eede2
Tola Chris Olarinre <olutolachris@gmail.com>
2022-09-15 15:23:27 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* _isupper - checks if a charactewr is upper or lower case
+* _isdigit - checks if a charactewr is upper or lower case
 *
 *@c: is the character to be checked
 * Return: Always Success
