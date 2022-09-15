093ce261532ad21dd4db60cb12bfe364fed515ef
Tola Chris Olarinre <olutolachris@gmail.com>
2022-09-15 15:49:03 -0700

    print_numbers

---
+++ 0x04-more_functions_nested_loops/3-prints_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_numbers - print number 0-9
+*
+* Return: Always
+*/
+	void print_numbers(void)
+{
+	char i = 48;
+		do {
+			_putchar(i);
+		} while (i < 58);
+		_putchar('\n');
+}
+
