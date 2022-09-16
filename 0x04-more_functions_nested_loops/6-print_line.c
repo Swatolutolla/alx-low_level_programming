7bed1366f1fb4c4a62dab66e2aaee8ccfe91684c
Tola Chris Olarinre <olutolachris@gmail.com>
2022-09-15 21:59:32 -0700

    more_numbers

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main"
+/**
+* print_line - prints a line
+*
+*@n: number of lone
+*Return: Always Success
+*/
+void print_line(int n)
+{
+	int j = 0;
+	while (j < n)
+	{
+		putchar('_');
+		j++;
+	}
+}
