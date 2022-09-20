5aee9e599b2d99c285818007857c696c9ea38289
Tola Chris Olarinre <olutolachris@gmail.com>
2022-09-20 13:08:54 -0700

    putd

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,17 +7,9 @@
 */
 void _puts(char *str)
 {
-	int len = 0;
 	int i;
 
-	while (*str != '\0')
-	{
-		str++;
-		len++;
-	}
-
-	str = str - len;
-	for (i = 0; i <= len; i++)
+	for (i = 0; *str != '\0'; i++)
 	{
 		_putchar(*(str + i));
 	}
