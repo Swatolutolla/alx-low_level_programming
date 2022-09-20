86c1ead54178fbd98e1885184249fa4cf12cab6b
Tola Chris Olarinre <olutolachris@gmail.com>
2022-09-20 12:40:09 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* _puts - print a string to the stdout
+*
+*@str: is the point to the string
+* Return: Always Success
+*/
+void _puts(char *str)
+{
+	int len = 0;
+
+	while (*str != '\0')
+	{
+		str++;
+		len++;
+	}
+
+	str = str - len;
+	for (int i = 0; i <= len; i++)
+	{
+		_putchar(*(str + i));
+	}
+}
+
