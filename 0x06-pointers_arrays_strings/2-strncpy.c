9b0debf1cc2fb6fd36eb55f6d634de3ac5b1a5ab
Tola Chris Olarinre <olutolachris@gmail.com>
2022-09-22 07:06:10 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strncpy - concatenates two strings
+*
+*@dest: pointer to the first string
+*@src: pointer to the second screen
+*@n: is the maximum bytes from
+* Return: Always dest (Success)
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int k = strlen(src);
+	int i;
+
+	for (i = 0; (i < n && i <= k); i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
