4b8ec2ef533df36fc25fda520846809f2da7911c
Tola Chris Olarinre <olutolachris@gmail.com>
2022-09-22 04:57:51 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strncat - concatenates two strings
+*
+*@dest: pointer to the first string
+*@src: pointer to the second screen
+*@n: is the maximum bytes from
+* Return: Always dest (Success)
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int  j = strlen(dest);
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[j + i] = src[i];
+	}
+	dest[j + i] = '\0';
+	return (dest);
+}
