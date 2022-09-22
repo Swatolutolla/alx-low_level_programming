e6439202d9217e518dde3a7521caaf85fd00adb0
Tola Chris Olarinre <olutolachris@gmail.com>
2022-09-22 07:19:07 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,12 +11,13 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int k = strlen(src);
+	int j = strlen(dest);
 	int i;
 
 	for (i = 0; (i < n && i <= k); i++)
 	{
 		dest[i] = src[i];
 	}
-	dest[i] = '\0';
+	dest[j + i] = '\0';
 	return (dest);
 }
