d5ed2cc87225112d4edb721eed0b23e5df0d77cd
Tola Chris Olarinre <olutolachris@gmail.com>
2022-09-23 16:59:53 -0700

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,27 +1,20 @@
 #include "main.h"
 /**
-* _strcmp - comapare two functions
-*
-*@s1: the first string to be comapred
-*@s2: the second string to be compared
-* Return: Always Success the Diffrence
+* _strcmp - Compares two strings
+* @s1: first string
+* @s2: second string
+* Return: An integer (0 if equal, +ve if s1 > s2, _ve if s1 < s2)
 */
 int _strcmp(char *s1, char *s2)
 {
-	int sum = 0;
-	int sum2 = 0;
-	int i;
-	int diffrence;
+int count = 0, count2 = 0;
 
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		sum += s1[i];
-	}
-	for (i = 0; s2[i] != '\0'; i++)
-	{
-		sum2 += s2[i];
-	}
-	diffrence = sum - sum2;
-	return (diffrence);
+while (count == 0)
+{
+if (*(s1 + count2) == '\0' && *(s2 + count2) == '\0')
+break;
+count = *(s1 + count2) - *(s2 + count2);
+count2++;
 }
-
+return (count);
+}
