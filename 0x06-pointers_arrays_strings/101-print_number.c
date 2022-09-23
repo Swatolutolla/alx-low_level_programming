0ffdc78d265546506a77e556766be93a7cdf8c52
Tola Chris Olarinre <olutolachris@gmail.com>
2022-09-22 18:05:52 -0700

    print

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_number - print and integer
+*
+*@n: is the integer to be printed
+* Return: Always Success
+*/
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+	{
+	print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
+}
