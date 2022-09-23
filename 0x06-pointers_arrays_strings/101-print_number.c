74a5d30f5093f10e96d55969c785e8b60a346795
Tola Chris Olarinre <olutolachris@gmail.com>
2022-09-22 18:10:42 -0700

    print

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,14 +7,17 @@
 */
 void print_number(int n)
 {
+	unsigned int j = 0;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	if (n / 10)
+	j = n;
+	if (j / 10)
 	{
-	print_number(n / 10);
+	print_number(j / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar(j % 10 + '0');
 }
