f20b512603dd4c443ee50029808caeedb2499b98
Tola Chris Olarinre <olutolachris@gmail.com>
2022-09-27 12:59:05 -0700

    chesse_board

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* print_chessboard - print a chess board
+*
+*@a: is the pointer of the arrays
+* Return: Always Success
+*/
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		if (i < 7)
+		{
+		_putchar('\n');
+		}
+	}
+}
