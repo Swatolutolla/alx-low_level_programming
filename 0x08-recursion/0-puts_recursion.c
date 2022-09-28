e2514d7a3607729428a6c1fccaf9778f761cc9c4
Tola Chris Olarinre <olutolachris@gmail.com>
2022-09-28 16:11:05 -0700

    put_recurssion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,18 +7,14 @@
 */
 void _puts_recursion(char *s)
 {
-	static int i;
-
-	i = 0;
-	if (s[i] == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
 	else
 	{
-		_putchar(s[i]);
-		s++;
-		_puts_recursion(s);
+		_putchar(*s);
+		_puts_recursion(++s);
 	}
 }
