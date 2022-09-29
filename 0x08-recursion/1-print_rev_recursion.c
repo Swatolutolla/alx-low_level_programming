80e599acb18ccd65c0745979fd0923fbcf95b68e
Tola Chris Olarinre <olutolachris@gmail.com>
2022-09-28 17:35:22 -0700

    print_rev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+* _print_rev_recursion - print strings using recursion
+*
+*@s: is the string to be reversersed
+* Return: Always Success
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		s++;
+		_print_rev_recursion(s);
+		_putchar(*s);
+	}
+}
+
+
