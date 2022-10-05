26c55c58a38af64eafc909e15819e135be75b35c
Tola Chris Olarinre <olutolachris@gmail.com>
2022-10-05 12:18:52 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+*@argc: command line argument counter
+*@argv: command line argument string array
+* Return: Always 0 (Success)
+*/
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d", argc);
+	return (0);
+}
