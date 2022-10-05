8b0f78a029fae97cc331f915851650c92dd82c2b
Tola Chris Olarinre <olutolachris@gmail.com>
2022-10-05 11:43:42 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+*@argc: argument counter
+*@argv: argument vector
+* Return: Always O (Success)
+*/
+int main(__attribute__((unused)) int argc, char **argv)
+{
+	putchar(*argv[0]);
+	return (0);
+}
