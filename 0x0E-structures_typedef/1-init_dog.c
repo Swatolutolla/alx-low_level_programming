fb3739f985259706e399d1e032953c6791f78275
Tola Chris Olarinre <olutolachris@gmail.com>
2022-10-10 19:52:04 -0700

    dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+* init_dog - initializes the structure dig
+* @d: is the objectvif the structure d
+* @name: is the name of the dog
+* @age: is the age of the dog
+* @owner: is the owner of the dog
+* Return: Always Success
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
+}
